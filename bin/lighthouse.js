38e48e93528f75c077948affe1dcecb13518dbdb
Rutger Broerze <contact@ruer.nl>
2024-10-22 13:05:35 +0200

    Update lighthouse.js

--- bin/lighthouse.js
+++ bin/lighthouse.js
@@ -1,47 +1,35 @@
 (async () => {
+    const chromeLauncher = await import('chrome-launcher');
+    const lighthouse = await import('lighthouse');
+
+    const inputArgs = JSON.parse(process.argv.slice(2));
+    const requestedUrl = inputArgs[0];
+
+    const chrome = await chromeLauncher.launch(inputArgs[1]);
+
+    const lighthouseOptions = {
+        logLevel: 'info',
+        port: chrome.port,
+    };
+
+    const lighthouseConfig = inputArgs[2];
+    const timeoutInMs = inputArgs[3];
+
+    const killTimer = setTimeout(() => {
+        chrome.kill();
+    }, timeoutInMs);
+
     try {
-        const chromeLauncher = await import('chrome-launcher');
-        const lighthouse = await import('lighthouse');
-
-        const inputArgs = JSON.parse(process.argv.slice(2));
-        const requestedUrl = inputArgs[0];
-
-        const chrome = await chromeLauncher.launch(inputArgs[1]);
-
-        const lighthouseOptions = {
-            logLevel: 'info',
-            port: chrome.port,
-        };
-
-        const lighthouseConfig = inputArgs[2];
-        const timeoutInMs = inputArgs[3];
-
-        const killTimer = setTimeout(() => {
-            chrome.kill();
-        }, 120000);
-
-        try {
-            const runnerResult = await lighthouse.default(
-                requestedUrl,
-                lighthouseOptions,
-                lighthouseConfig
-            );
-
-            process.stdout.write(JSON.stringify(runnerResult));
-        } catch (err) {
-            const errorRunnerResult = {
-                error: err.message,
-            };
-
-            process.stdout.write(JSON.stringify(errorRunnerResult));
-        } finally {
-            clearTimeout(killTimer);
-            await chrome.kill();
-        }
-    } catch (error) {
-        const errorResult = {
-            error: "Unexpected error in script: " + error.message,
-        };
-        process.stdout.write(JSON.stringify(errorResult));
+        const runnerResult = await lighthouse.default(
+            requestedUrl,
+            lighthouseOptions,
+            lighthouseConfig
+        );
+        process.stdout.write(JSON.stringify(runnerResult));
+    } catch (err) {
+        console.error("Lighthouse failed:", err);
+    } finally {
+        clearTimeout(killTimer);
+        await chrome.kill();
     }
 })();
